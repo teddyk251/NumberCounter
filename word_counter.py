11dbb489d19e23c5f11467f5d0ced9f20b28386a
Tewodros Kederalah <teddylnk1@gmail.com>
2019-04-20 11:31:39 +0000

    Added implementation of the word counter method

--- word_counter.py
+++ word_counter.py
@@ -1,3 +1,5 @@
+import os.path
+
 def count_words(string=None,file=None):
     words = 0
     if string:
@@ -6,3 +8,13 @@ def count_words(string=None,file=None):
         with open(file) as f:
             words = len(f.read().split())
     return words
+
+
+
+count = input("Enter a string or a file path: ")
+if os.path.isfile(count):
+    words = count_words(None,count)
+    print("The number of words in the file are: ", words)
+else:
+    words = count_words(count,None)
+    print("The number of words in the string are: ", words)
